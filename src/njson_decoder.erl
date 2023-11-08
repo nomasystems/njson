cff28a133eea2688d573efcb63c66e3ee4ba12f1
Miguel Rubinos Rodríguez <mrrubinos@gmail.com>
2023-11-08 16:44:04 +0100

    Update src/njson_decoder.erl
    Co-authored-by: Javier Garea <javigarea@gmail.com>

--- src/njson_decoder.erl
+++ src/njson_decoder.erl
@@ -212,7 +212,7 @@ key(<<C, Bin/binary>>, Original, Skip, Next) ->
     Binary :: binary(),
     Skip :: non_neg_integer(),
     Next :: [?END | ?KEY | ?OBJECT | ?ARRAY | list() | map()],
-    Value :: false | null | true | binary() | number() | list() | map(),
+    Value :: njson:t(),
     OK :: {ok, Json},
     Json :: njson:t(),
     Error :: njson:decode_error().
